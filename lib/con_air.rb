edaa992f4a870b39462c88807c58cdc2c02f6a28
tubaxenor <tubaxenor@gmail.com>
2013-12-16 16:43:54 +0800

    Add hijack on per model

--- lib/con_air.rb
+++ lib/con_air.rb
@@ -10,4 +10,12 @@ module ConAir
   ensure
     ar.connection_hijackings[ar.connection_id] = nil
   end
+
+  def self.hijack_on(klass, conn, &block)
+    klass.connection_hijackings["#{klass}_#{klass.connection_id}"] = conn
+
+    yield
+  ensure
+    klass.connection_hijackings["#{klass}_#{klass.connection_id}"] = nil
+  end
 end
